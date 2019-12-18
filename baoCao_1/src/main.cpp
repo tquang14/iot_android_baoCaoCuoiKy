72dc8321dfa8596d5a0d5bd626bd18cee7de55e6
tquang14 <44432368+tquang14@users.noreply.github.com>
2019-12-18 15:57:51 +0700

    update_mode_auto

--- baoCao_1/src/main.cpp
+++ baoCao_1/src/main.cpp
@@ -7,15 +7,19 @@
 #include <FirebaseArduino.h>
 
 DHT dht(D3, DHT11);
-float t = 0;
-float h = 0;
+// khai bao cac bien luu gia tri cam bien va cac bien chua gia tri de bat tat thiet bi
+float t = 0, t_auto = 0;
+float h = 0, h_auto = 0;
 float doAm_dat = 0;
-int percent_doAm_dat = 0;
-float light = 0;
+int percent_doAm_dat = 0, percent_doAm_dat_auto = 0;
+float light = 0, light_auto = 0;
+// cac bien trang thai
 String bom_state = "off";
 String phunSuong_state = "off";
 String den_state = "off";
 String mua = "false";
+String mode = "manual";
+
 BH1750 lightMeter(0x23);
 #define FIREBASE_HOST "testmyled-aa8f7.firebaseio.com"
 #define FIREBASE_AUTH "gn0qcrclXBS7gVjNSNpOaULUn17H4cssN2kRxhie"
@@ -23,6 +27,7 @@ BH1750 lightMeter(0x23);
 void control_relay(String relay1, String relay2, String relay3, String relay4);
 void get_state_control();
 void set_to_firebase(String anhSang, String doAmDat, String doAmKK, String nhietDo, String mua);
+void get_auto_val();
 
 void setup() {
   // put your setup code here, to run once:
@@ -55,27 +60,51 @@ void loop() {
   doAm_dat = analogRead(A0);
   percent_doAm_dat = 100 - map(doAm_dat,0, 1024, 0, 100);
   light = lightMeter.readLightLevel();
-  Serial.println(h);
-  Serial.println(t);
-  Serial.println(percent_doAm_dat);
-  Serial.print("anh sang: ");
-  Serial.println(light);
   // kiem tra mua
   if (!digitalRead(D7)) {
     mua = "true";
-    Serial.println("mua");
   } else {
     mua = "false";
-    Serial.println("ko mua");
   }
-  
   set_to_firebase(String(light), String(percent_doAm_dat), String(h), String(t), mua);
+  
+  // neu che do hoat dong la auto
+  if (mode == "auto") {
+    // lay cac gia tri dk tu dong
+    get_auto_val();
+    // kiem tra nhiet do kk
+    if (t_auto > t) { 
+      Firebase.setString("iot_android_control/den", "on");
+    } else {
+      Firebase.setString("iot_android_control/den", "off");
+    }
+    // kiem tra do am dat
+    if (percent_doAm_dat_auto > percent_doAm_dat) {
+      Firebase.setString("iot_android_control/bom", "on");
+    } else {
+      Firebase.setString("iot_android_control/bom", "off");
+    }
+    // kiem tra do am kk
+    if (h_auto > h) {
+      Firebase.setString("iot_android_control/phunSuong", "on");
+    } else {
+      Firebase.setString("iot_android_control/phunSuong", "off");
+    }
+    // neu anh sang cai dat < anh sang hien tai => dong mai nha
+    if (light < light_auto) {
+      // sang led
+      Serial.println("sang led");
+    } else {
+      // tat led
+      Serial.println("tat led");
+    }
+    delay(5000);
+  }
   // lay trang thai cac relay tren firebase
   get_state_control();
   // dieu khien bom
   control_relay(bom_state, phunSuong_state, den_state, "off"); // relay 4 ko dung
-  if (bom_state)
-  delay(5000);
+  delay(1000);
 }
 
 void set_to_firebase(String anhSang, String doAmDat, String doAmKK, String nhietDo, String mua) {
@@ -89,6 +118,19 @@ void get_state_control() {
   bom_state = Firebase.getString("iot_android_control/bom");
   phunSuong_state = Firebase.getString("iot_android_control/phunSuong");
   den_state = Firebase.getString("iot_android_control/den");
+  mode = Firebase.getString("iot_android_control/mode");
+}
+
+void get_auto_val() {
+  // lay thong tin tu firebase va ep sang kieu float
+  light_auto = Firebase.getString("auto_val/anhSang").toFloat();
+  percent_doAm_dat_auto = Firebase.getString("auto_val/doAmDat").toInt();
+  h_auto = Firebase.getString("auto_val/doAmKK").toFloat();
+  t_auto = Firebase.getString("auto_val/nhietDo").toFloat();
+  Serial.println(light_auto);
+  Serial.println(percent_doAm_dat_auto);
+  Serial.println(h_auto);
+  Serial.println(t_auto);
 }
 
 void control_relay(String relay1, String relay2, String relay3, String relay4) {
